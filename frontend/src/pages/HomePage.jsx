c13fbd21abd68c7cb013aaccbd13e784a1602e1e
Muavia-Shakeel <muaviashakeel70@gmail.com>
2025-01-31 16:02:49 +0500

    Set up React folder structure and Homepage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+const Homepage = () => {
+    return (
+        <div>
+            <h1>To-DO List APP</h1>
+            <p>Welcome! Start managing your tasks.</p>
+        </div>
+    );
+};
+
+export default Homepage;
